ccedb31221cec12b23ab64e16a9810d8410fc6f3
Shuai Lin <linshuai2012@gmail.com>
2016-11-29 15:32:21 +0800

    More tests in travis builds.

--- ci/ci.sh
+++ ci/ci.sh
@@ -11,9 +11,10 @@ pip install docker-squash
 
 sudo cp samples/server.conf bootstrap/bootstrap.conf
 
-sudo ./launcher bootstrap
-sudo ./launcher start && sleep 10
+sudo ./launcher -v bootstrap
+sudo ./launcher -v start && sleep 10
 sudo ./launcher stop --skip-prereqs
 sudo ./launcher start --docker-args "--memory 1g" && sleep 10
 sudo ./launcher restart
-sudo ./launcher rebuild --docker-args "--memory 1g"
+sudo ./launcher -v rebuild
+sudo ./launcher -v rebuild --docker-args "--memory 1g"
