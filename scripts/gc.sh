b2b7a7d41562f90df5e232bcbb0c2d45c3b1c84c
Shuai Lin <linshuai2012@gmail.com>
2017-06-06 18:15:18 +0800

    Preseve the exit code of seaf-gc.sh

--- scripts/gc.sh
+++ scripts/gc.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
+set -e
+
+# Before
 SEAFILE_DIR=/opt/seafile/seafile-server-latest
 
-# Stop the seafile server in order to perform a cleanup
 $SEAFILE_DIR/seafile.sh stop
 
-# Wait to make sure the server has been shutdown completly
-echo Giving the server some time to shut down properly....
-sleep 10
+echo "Waiting for the server to shut down properly..."
+sleep 5
 
-# Start the garbage collector and pipe its output to a log file
-$SEAFILE_DIR/seaf-gc.sh | tee -a /var/log/gc.log
+# Do it
+(
+    set +e
+    $SEAFILE_DIR/seaf-gc.sh | tee -a /var/log/gc.log
+    # We want to presevent the exit code of seaf-gc.sh
+    exit "${PIPESTATUS[0]}"
+)
 
-# Wait a few seconds to make sure everything is finished
-echo Giving the server some time....
+gc_exit_code=$?
+
+# After
+
+echo "Giving the server some time..."
 sleep 3
 
-# Restart the seafile server
 $SEAFILE_DIR/seafile.sh start
+
+exit $gc_exit_code
