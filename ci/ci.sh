7af74ce3f31a5f4058de324f691d65d96c15cf7a
root <517046497@qq.com>
2018-04-03 05:26:38 +0000

    travis

--- ci/ci.sh
+++ ci/ci.sh
@@ -12,13 +12,13 @@ set -e -x
 
 sudo cp samples/server.conf bootstrap/bootstrap.conf
 
-sudo ./launcher -v bootstrap
 sudo ./launcher -v start && sleep 10
 sudo ./launcher stop --skip-prereqs
 sudo ./launcher start --docker-args "--memory 1g" && sleep 10
 sudo ./launcher restart
 sudo ./launcher -v rebuild
 sudo ./launcher -v rebuild --docker-args "--memory 1g"
+
 
 if [[ $TRAVIS_TAG != "" ]]; then
     ci/publish-image.sh
