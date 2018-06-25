c449b529a578caaffe98683249af5bd5dcca6e10
root <517046497@qq.com>
2018-06-25 07:05:09 -0400

    update time_zone changeable

--- scripts/create_data_links.sh
+++ scripts/create_data_links.sh
@@ -7,6 +7,17 @@ if [[ $SEAFILE_BOOTSRAP != "" ]]; then
     exit 0
 fi
 
+if [[ $TIME_ZONE != "" ]]; then
+    time_zone=/usr/share/zoneinfo/$TIME_ZONE
+    if [[ ! -e $time_zone ]]; then
+        echo "invalid time zone"
+        exit 1
+    else
+        ln -snf $time_zone /etc/localtime
+        echo "$TIME_ZONE" > /etc/timezone
+    fi
+fi
+
 dirs=(
     conf
     ccnet
