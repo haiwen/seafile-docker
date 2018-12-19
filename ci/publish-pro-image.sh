c010853a5248e6bcc656ce17f7eb24aa03c5d321
Hulk <feipeng.ren@seafile.com>
2018-12-19 10:26:27 +0100

    publish images from 16.04 to 18.04

--- ci/publish-pro-image.sh
+++ ci/publish-pro-image.sh
@@ -14,8 +14,8 @@ docker login -u="$DOCKER_PRO_REGISTRY_USER" -p="$DOCKER_PRO_REGISTRY_PASSWORD" d
 ## Always use the base image we build manually to reduce the download size of the end user.
 docker rm -f $(docker ps -a -q)
 docker rmi -f $(docker images -a -q)
-docker pull docker.seadrive.org/seafileltd/pro-base:16.04
-docker tag docker.seadrive.org/seafileltd/pro-base:16.04 seafileltd/pro-base:16.04
+docker pull docker.seadrive.org/seafileltd/pro-base:18.04
+docker tag docker.seadrive.org/seafileltd/pro-base:18.04 seafileltd/pro-base:18.04
 
 (
     cd image
@@ -27,8 +27,8 @@ docker tag docker.seadrive.org/seafileltd/pro-base:16.04 seafileltd/pro-base:16.
 docker login -u="$LOCAL_DOCKER_PRO_REGISTRY_USER" -p="$LOCAL_DOCKER_PRO_REGISTRY_PASSWORD" docker.seafile.top
 
 docker rmi -f $(docker images | awk {'print $3'})
-docker pull docker.seafile.top/seafileltd/pro-base:16.04
-docker tag docker.seafile.top/seafileltd/pro-base:16.04 seafileltd/pro-base:16.04
+docker pull docker.seafile.top/seafileltd/pro-base:18.04
+docker tag docker.seafile.top/seafileltd/pro-base:18.04 seafileltd/pro-base:18.04
 (
     cd image
     make host=docker.seafile.top pro-server push-pro-server
