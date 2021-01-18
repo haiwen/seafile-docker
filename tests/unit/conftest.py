2a30a0385b58e3fa7397bb7c0fed600eddfe66e2
Gerrit Gogel <gerrit@gogel.me>
2021-01-18 16:00:39 +0100

    cleanup and move

--- tests/unit/conftest.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-from os.path import abspath, basename, exists, dirname, join
-
-import pytest
-
-project_dir = join(dirname(abspath(__file__)), '../..')
-scripts_dir = join(project_dir, 'scrpits')
