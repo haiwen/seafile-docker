aa85043e2338a4647970d1878e9930b40579b0d0
Gerrit Gogel <gerrit@gogel.me>
2021-01-22 23:33:28 +0100

    seafile-server wait until searpc is available

--- seafile/scripts/start.sh
+++ seafile/scripts/start.sh
@@ -2,7 +2,11 @@
 
 /scripts/create_data_links.sh
 
-mkdir -p /opt/seafile/seafile-server-latest/runtime
-socat -v -d -d TCP-LISTEN:8001,fork,forever UNIX:/opt/seafile/seafile-server-latest/runtime/seafile.sock,forever &
+python3 /scripts/start.py &
 
-python3 /scripts/start.py
+while [ ! -S /opt/seafile/seafile-server-latest/runtime/seafile.sock ]; do
+    echo "Waiting for SeaRPC socket..."
+    sleep 1
+done
+
+socat -v -d -d TCP-LISTEN:8001,fork UNIX:/opt/seafile/seafile-server-latest/runtime/seafile.sock,forever
