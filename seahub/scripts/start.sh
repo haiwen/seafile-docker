9c0f7e2bd26e905360c37e594db79c727f4b3482
Gerrit Gogel <gerrit@gogel.me>
2021-01-22 23:33:02 +0100

    seahub change socat cmd

--- seahub/scripts/start.sh
+++ seahub/scripts/start.sh
@@ -3,7 +3,7 @@
 /scripts/create_data_links.sh
 
 mkdir -p /opt/seafile/seafile-server-latest/runtime
-socat -v -d -d UNIX-LISTEN:/opt/seafile/seafile-server-latest/runtime/seafile.sock,forever,fork TCP:seafile-server:8001,forever &
+socat -v -d -d UNIX-LISTEN:/opt/seafile/seafile-server-latest/runtime/seafile.sock,fork TCP:seafile-server:8001,forever &
 
 python3 /opt/seafile/seafile-server-latest/seahub/manage.py runserver 0.0.0.0:8000
 
