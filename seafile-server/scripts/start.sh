dba73bc89ff623a0855d46f0abb92d7575e76080
Gerrit Gogel <gerrit@gogel.me>
2021-01-24 16:01:09 +0100

    dial down socat verbose output

--- seafile-server/scripts/start.sh
+++ seafile-server/scripts/start.sh
@@ -9,4 +9,4 @@ while [ ! -S /opt/seafile/seafile-server-latest/runtime/seafile.sock ]; do
     sleep 1
 done
 
-socat -v -d -d -d TCP-LISTEN:8001,fork,reuseaddr UNIX:/opt/seafile/seafile-server-latest/runtime/seafile.sock,forever,keepalive
+socat -d -d TCP-LISTEN:8001,fork,reuseaddr UNIX:/opt/seafile/seafile-server-latest/runtime/seafile.sock,forever,keepalive
