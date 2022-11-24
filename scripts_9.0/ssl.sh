1651ca4ad871b2f6c0532937ec021ba739b57185
Kiril Isakov <kirisakow@users.noreply.github.com>
2022-11-24 08:09:35 +0100

    Use $ssldir instead of hardcoded literals. Make sure $ssldir ends in single '/'

--- scripts_9.0/ssl.sh
+++ scripts_9.0/ssl.sh
@@ -3,19 +3,20 @@
 set -e
 
 ssldir=${1:?"error params"}
+ssldir=$(echo "$ssldir/" | sed 's|//|/|')
 domain=${2:?"error params"}
 
 mkdir -p /var/www/.well-known/acme-challenge/
 chmod 755 /var/www/.well-known/acme-challenge/
 ln -sf /var/www/.well-known/acme-challenge/ /var/www/challenges
 
-domain_num=$(/root/.acme.sh/acme.sh --home /shared/ssl/ --list | grep "$domain" | grep -v "grep" | wc -l)
+domain_num=$(/root/.acme.sh/acme.sh --home ${ssldir} --list | grep "$domain" | grep -v "grep" | wc -l)
 
 if [ $domain_num -eq 0 ]; then
-    /root/.acme.sh/acme.sh --debug --issue --home /shared/ssl/ --server letsencrypt -d ${domain} -w /var/www/
-    /root/.acme.sh/acme.sh --home /shared/ssl/ --install-cert -d ${domain} --key-file /shared/ssl/${domain}.key --fullchain-file /shared/ssl/${domain}.crt
+    /root/.acme.sh/acme.sh --debug --issue --home ${ssldir} --server letsencrypt -d ${domain} -w /var/www/
+    /root/.acme.sh/acme.sh --home ${ssldir} --install-cert -d ${domain} --key-file ${ssldir}${domain}.key --fullchain-file ${ssldir}${domain}.crt
 else
-    /root/.acme.sh/acme.sh --debug --home /shared/ssl/ --renew -d ${domain} --days 60
+    /root/.acme.sh/acme.sh --debug --home ${ssldir} --renew -d ${domain} --days 60
 fi
 
 nginx -s reload
