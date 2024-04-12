8d94946b551d09c36e09fcc3c052830ce529dcff
skywalker <dzmbbs@qq.com>
2024-04-12 16:47:29 +0800

    add mysql-client

--- scripts/scripts_11.0/utils.py
+++ scripts/scripts_11.0/utils.py
@@ -285,13 +285,14 @@ def wait_for_mysql():
 
     while True:
         try:
-            connection = pymysql.connect(host=db_host, port=db_port, user=db_user, passwd=db_passwd)
+            # use mysql-client instead of pymysql
+            cmd = 'mysqladmin ping -h %s -P %s -u %s --password=%s' % (db_host, db_port, db_user, db_passwd)
+            call(cmd)
         except Exception as e:
-            print ('waiting for mysql server to be ready: %s', e)
+            print ('waiting for mysql server to be ready:', e)
             time.sleep(2)
             continue
         logdbg('mysql server is ready')
-        connection.close()
         return
 
 def wait_for_nginx():
