4d833c4da5a81e8548cdb5a20f2cd4bea32e65a3
skywalker <dzmbbs@qq.com>
2024-08-07 14:38:22 +0800

    update generate SERVICE_URL

--- scripts/scripts_11.0/setup-seafile-mysql.py
+++ scripts/scripts_11.0/setup-seafile-mysql.py
@@ -1037,6 +1037,14 @@ class SeahubConfigurator(AbstractConfigurator):
     def ask_questions(self):
         pass
 
+    def get_proto(self):
+        is_https = os.environ.get('SEAFILE_SERVER_LETSENCRYPT', 'false').lower() == 'true'
+        proto = 'https' if is_https else 'http'
+        force_https_in_conf = os.environ.get('FORCE_HTTPS_IN_CONF', 'false').lower() == 'true'
+        if force_https_in_conf:
+            proto = 'https'
+        return proto
+
     def generate(self):
         '''Generating seahub_settings.py'''
         print('Generating seahub configuration ...\n')
@@ -1045,7 +1053,7 @@ class SeahubConfigurator(AbstractConfigurator):
             fp.write('\n')
             self.write_secret_key(fp)
             fp.write('\n')
-            fp.write('SERVICE_URL = "http://%s"' % ccnet_config.ip_or_domain)
+            fp.write('SERVICE_URL = "%s://%s"' % (self.get_proto(), ccnet_config.ip_or_domain))
             fp.write('\n')
             self.write_database_config(fp)
 
