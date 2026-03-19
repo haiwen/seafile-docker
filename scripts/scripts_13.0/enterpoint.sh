f49f977435328340fd6526f922f1a4e6da080770
Michael Schuster <michael@schuster.ms>
2026-03-19 02:16:57 +0100

    CE 13.0: Refactor non-root handling in enterpoint.sh
     enterpoint.sh:
     - create missing directories, if needed
     - allow the user to set custom `UID` and `GID` for the container-user `seafile` via optional parameters (default remains 8000:8000)
     - (re)set the same secure permissions the volume folders initially had with the standard root-user setup
     - only change the owner recursively if the main folders have another owner, to avoid unnecessary overhead with large storages
     - leave the app folders and binaries untouched but grant write access to `/opt/seafile` (e.g. for creating the `pids` folder) and the `runtime` folder, in the container
    
     bootstrap.py:
      - remove the chmod code block in init_seafile_server()

--- scripts/scripts_13.0/enterpoint.sh
+++ scripts/scripts_13.0/enterpoint.sh
@@ -24,29 +24,92 @@ done
 
 # non-noot
 if [[ $NON_ROOT == "true" ]]; then
-    log "Create linux user seafile in container, please wait."
-    groupadd --gid 8000 seafile
-    useradd --home-dir /home/seafile --create-home --uid 8000 --gid 8000 --shell /bin/sh --skel /dev/null seafile
+    log "Setting up non-root user seafile in container, please wait."
 
-    if [[ -e /shared/seafile/ ]]; then
-        permissions=$(stat -c %a "/shared/seafile/")
-        owner=$(stat -c %U "/shared/seafile/")
-        if [[ $permissions != "777" && $owner != "seafile" ]]; then
-            log "The permission of path seafile/ is incorrect."
-            log "To use non root, change the folder permission of seafile folder in your host machine by 'chmod -R a+rwx /opt/seafile-data/' and try again later. (If you use another path, change the path in the command correspondingly). Now quit."
-            exit 1
-        fi
+    # configurable UID/GID (optional parameters)
+    SEAFILE_UID=${NON_ROOT_UID:-8000}
+    SEAFILE_GID=${NON_ROOT_GID:-8000}
+
+    # create group and user if they don't exist
+    if ! getent group seafile >/dev/null; then
+        log "Creating group seafile with GID $SEAFILE_GID."
+        groupadd --gid "$SEAFILE_GID" seafile
+    fi
+    if ! id seafile >/dev/null 2>&1; then
+        log "Creating user seafile with UID $SEAFILE_UID."
+        useradd --uid "$SEAFILE_UID" --gid "$SEAFILE_GID" \
+                --home-dir /home/seafile --create-home \
+                --shell /bin/sh --skel /dev/null seafile
     fi
 
-    # chown
-    chown seafile:seafile /opt/seafile/
-    chown -R seafile:seafile /opt/seafile/$SEAFILE_SERVER-$SEAFILE_VERSION/
+    DATA_DIR="/shared/seafile"
+
+    # data directories that must be writable
+    WRITABLE_DATA_DIRS=(
+        "$DATA_DIR"
+        "$DATA_DIR/conf"
+        "$DATA_DIR/logs"
+        "$DATA_DIR/pro-data"
+        "$DATA_DIR/seafile-data"
+        "$DATA_DIR/seahub-data"
+    )
+
+    for dir in "${WRITABLE_DATA_DIRS[@]}"; do
+        if [ ! -d "$dir" ]; then
+            log "Creating missing directory $dir"
+            mkdir -p "$dir"
+        fi
+
+        if [[ $dir == "$DATA_DIR" || \
+              $dir == "$DATA_DIR/seahub-data" ]]; then
+            new_perms=755
+        else
+            new_perms=700
+        fi
+
+        # check permissions
+        owner=$(stat -c %U "$dir")
+        perms=$(stat -c %a "$dir")
+        if [[ "$owner" != "seafile" ]]; then
+            log "Adjusting ownership of $dir -> seafile"
+            if [[ $dir == "$DATA_DIR" ]]; then
+                chown seafile:seafile "$dir"
+            else
+                chown -R seafile:seafile "$dir"
+            fi
+        fi
+        if [[ "$perms" != "$new_perms" ]]; then
+            log "Adjusting permissions of $dir -> $new_perms"
+            chmod $new_perms "$dir"
+        fi
+    done
+
+    # container directories that must be writable
+    # leave app binaries root-owned for safety
+    WRITABLE_SERVER_DIRS=(
+        "/opt/seafile"
+        "/opt/seafile/$SEAFILE_SERVER-$SEAFILE_VERSION/runtime"
+    )
+    new_perms=1755
+
+    for dir in "${WRITABLE_SERVER_DIRS[@]}"; do
+        # check permissions
+        owner=$(stat -c %U "$dir")
+        perms=$(stat -c %a "$dir")
+        if [[ "$owner" != "seafile" ]]; then
+            log "Adjusting ownership of $dir -> seafile"
+            chown seafile:seafile "$dir"
+        fi
+        if [[ "$perms" != "$new_perms" ]]; then
+            log "Adjusting permissions of $dir -> $new_perms"
+            chmod $new_perms "$dir"
+        fi
+    done
 
     # logrotate
     sed -i 's/^        create 644 root root/        create 644 seafile seafile/' /scripts/logrotate-conf/seafile
 
-    # seafile.sh
-    sed -i 's/^    validate_running_user;/#    validate_running_user;/' /opt/seafile/$SEAFILE_SERVER-$SEAFILE_VERSION/seafile.sh
+    log "Finished non-root setup."
 fi
 
 # logrotate
